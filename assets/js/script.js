619b9d45549a139ea4e1dbdd01230d623a9e5f61
AlenaGM <alena.guillaume4@gmail.com>
2022-02-09 13:38:24 +0100

    Corrections after tutor's check

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,46 @@
 //Запрашиваем инфо и обрезаем пробелы в начале и конце
 const fullName = prompt(`Введите ФИО`).trim();
-console.log(fullName);
 
 //Делим на части
-let namesSpaces = fullName.split(" ");
-console.log(namesSpaces);
+const namesSpaces = fullName.split(" ");
 
 function deleteSpaces(value) {//Удаляем пустые элементы = пробелы внутри ФИО
     return value != '';
 };
 
-let names = namesSpaces.filter(deleteSpaces);
-console.log(names);
-
+const names = namesSpaces.filter(deleteSpaces);
 
 if (names.length>2){//Стандартная ситуация: есть 1 фамилия, 1 имя и 1 отчество
 
     //Запихиваем в переменную и приводим в божеский вид
-    let smallFirstName = names[1].toLowerCase(); //мария
-    let userFirstName = smallFirstName[0].toUpperCase()+smallFirstName.slice(1);//Мария
-
-    console.log(userFirstName);
+    const smallFirstName = names[1].toLowerCase(); //мария
+    const userFirstName = smallFirstName[0].toUpperCase()+smallFirstName.slice(1);//Мария
 
     //Выводим результат в инпут
     document.querySelector('#userFirstName').value = userFirstName;
 
-    let smallPatronym = names[2].toLowerCase(); //викторовна
-    let userPatronym = smallPatronym[0].toUpperCase()+smallPatronym.slice(1);//Викторовна
-
-    console.log(userPatronym);
+    const smallPatronym = names[2].toLowerCase(); //викторовна
+    const userPatronym = smallPatronym[0].toUpperCase()+smallPatronym.slice(1);//Викторовна
 
     //Выводим результат в инпуты
     document.querySelector('#userPatronym').value = userPatronym;
 
-    let smallLastName = names[0].toLowerCase(); //петрова
-    let lastName = smallLastName[0].toUpperCase()+smallLastName.slice(1);//Петрова
+    const smallLastName = names[0].toLowerCase(); //петрова
+    const lastName = smallLastName[0].toUpperCase()+smallLastName.slice(1);//Петрова
 
-    let doubleLastName = lastName.split("-");//На случай двойной фамилии
+    const doubleLastName = lastName.split("-");//На случай двойной фамилии
 
             if (doubleLastName.length !=2){//Стандартная ситуация: одинарная фамилия
-                let userLastName= lastName;
-                console.log(userLastName);
+                const userLastName= lastName;
 
                 //Выводим результат в инпут
                 document.querySelector('#userLastName').value = userLastName;//Петрова
 
             } else {//Нестандартная ситуация: двойная фамилия через дефис
-                let secondLastName = doubleLastName[1];//водкина
-                let userSecondLastName = secondLastName[0].toUpperCase()+doubleLastName[1].slice(1);//Водкина
+                const secondLastName = doubleLastName[1];//водкина
+                const userSecondLastName = secondLastName[0].toUpperCase()+doubleLastName[1].slice(1);//Водкина
 
                 userLastName = doubleLastName[0] + "-" + userSecondLastName;
-                console.log(userLastName);
 
                 //Выводим результат в инпут
                 document.querySelector('#userLastName').value = userLastName;//Петрова-Водкина
@@ -59,33 +49,29 @@ if (names.length>2){//Стандартная ситуация: есть 1 фам
     } else {//Нестандартная ситуация: есть только 1 фамилия и 1 имя, отчества нет
 
         //Запихиваем в переменную и приводим в божеский вид
-        let smallFirstName = names[1].toLowerCase(); //мария
-        let userFirstName = smallFirstName[0].toUpperCase()+smallFirstName.slice(1);//Мария
-
-        console.log(userFirstName);
+        const smallFirstName = names[1].toLowerCase(); //мария
+        const userFirstName = smallFirstName[0].toUpperCase()+smallFirstName.slice(1);//Мария
 
         //Выводим результат в инпуты
         document.querySelector('#userFirstName').value = userFirstName;
         document.querySelector('#userPatronym').value = "";
 
-        let smallLastName = names[0].toLowerCase(); //петрова
-        let lastName = smallLastName[0].toUpperCase()+smallLastName.slice(1);//Петрова
+        const smallLastName = names[0].toLowerCase(); //петрова
+        const lastName = smallLastName[0].toUpperCase()+smallLastName.slice(1);//Петрова
 
-        let doubleLastName = lastName.split("-");//На случай двойной фамилии
+        const doubleLastName = lastName.split("-");//На случай двойной фамилии
 
             if (doubleLastName.length !=2){//Стандартная ситуация: одинарная фамилия
-                let userLastName= lastName;
-                console.log(userLastName);
+                const userLastName= lastName;
 
                 //Выводим результат в инпут
                 document.querySelector('#userLastName').value = userLastName;//Петрова
 
             } else {//Нестандартная ситуация: двойная фамилия через дефис
-                let secondLastName = doubleLastName[1];//водкина
-                let userSecondLastName = secondLastName[0].toUpperCase()+doubleLastName[1].slice(1);//Водкина
+                const secondLastName = doubleLastName[1];//водкина
+                const userSecondLastName = secondLastName[0].toUpperCase()+doubleLastName[1].slice(1);//Водкина
 
                 userLastName = doubleLastName[0] + "-" + userSecondLastName;
-                console.log(userLastName);
 
                 //Выводим результат в инпут
                 document.querySelector('#userLastName').value = userLastName;//Петрова-Водкина
@@ -94,9 +80,6 @@ if (names.length>2){//Стандартная ситуация: есть 1 фам
 
     document.querySelector("#confirm").addEventListener("click", function(event) {
         event.preventDefault();
-        console.log(userFirstName.value);//Просто проверить сохраняются ли исправленные значения
-        console.log(userPatronym.value);
-        console.log(userLastName.value);
 
         document.querySelector('#userFirstName').value = ''; //Очищаем форму после отправки
         document.querySelector('#userPatronym').value = '';
